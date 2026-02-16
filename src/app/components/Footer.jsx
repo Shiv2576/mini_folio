df590f7b8ee6f40aa1e531dffe58cbd4811e031e
Shivang <dixitshiva12358@gmail.com>
2026-02-17 02:13:54 +0530

    updated

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,8 @@
-import VisitorCounter from "./visitor";
-
 export default function Footer() {
   return (
     <div className="mt-20 p-4">
       <div className="flex justify-between items-center">
         <p className="text-sm">© 2025. All rights reserved.</p>
-        <VisitorCounter />
       </div>
       <p className="text-xs mt-2">
         This site follow{" "}
